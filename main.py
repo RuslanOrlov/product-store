13e60d2c3bf72011316d300b1d83c8d278f58d90
Ruslan Orlov <org1978org@gmail.com>
2026-09-08 15:32:40 +0500

    updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from fastapi import FastAPI
+# Ctrl + Shift + X  - открыть панель расширений с нужным ИИ помощником и
+#                     полностью отключить/включить (Disable / Enable)
+#                     (если не видно нужное расширение, в строке поиска
+#                     написать GitHub Copilot Chat или Fitten Code)
+
+# Ctrl + Shift + P  - открыть палитру команд и набрать GitHub Copilot: Disable/Enable
+#                     (или имя другого помощника), чтобы найти нужную команду
+#                     и нажать Enter, чтобы отключить/включить ИИ помощника
+
+# Для включения автозаполнения Fitten Code:
+# 1. нажать шестеренку слева снизу -> Settings ->
+# 2. ввести в поле поиска fitten ->
+# 3. включить галочку в поле "Enable inline code completion"
+
+# Закомментировать несколько строк  - Ctrl + K, затем Ctrl + C
+# Раскомментировать несколько строк - Ctrl + K, затем Ctrl + U
+
+from fastapi import FastAPI, Response
 
 app = FastAPI()
