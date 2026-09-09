de34234f2af4d7206ec4a0c9fc812c1ebdc1604f
Ruslan Orlov <org1978org@gmail.com>
2026-09-09 15:49:41 +0500

    added app/repositories/category.py, app/repositories/product.py and modified app/services/category.py

---
+++ app/repositories/category.py
@@ -0,0 +1,36 @@
+from app.schemas.category import CreateCategoryRequest, GetCategory
+
+
+def is_category_exists(name: str | None = None, id: int = 0) -> bool:
+    """
+    Проверяет, существует ли категория с заданным именем или идентификатором.
+    :param name: Имя категории для проверки.
+    :param id: Идентификатор категории для проверки.
+    :return: True, если категория существует, иначе False.
+    """
+    if name is not None:
+        pass  # Логика проверки существования категории по имени
+    elif id is not None:
+        pass  # Логика проверки существования категории по идентификатору
+    else:
+        raise ValueError("It is necessary to provide either a name or a category ID")
+
+
+def create_category(category: CreateCategoryRequest):
+    pass  # Логика создания категории
+
+
+def get_category_by_id(id: int):
+    pass  # Логика получения категории по идентификатору
+
+
+def get_all_categories():
+    pass  # Логика получения всех категорий
+
+
+def update_category(category: GetCategory):
+    pass  # Логика обновления категории
+
+
+def delete_category(id: int):
+    pass  # Логика удаления категории
--- app/services/category.py
+++ app/services/category.py
@@ -1,5 +1,73 @@
-from app.schemas.category import CreateCategoryRequest
+from fastapi import HTTPException
+
+from app.schemas.category import CreateCategoryRequest, GetCategory
+from app.repositories import category as category_repository
+from app.repositories import product as product_repository
 
 
 def create_category(category: CreateCategoryRequest):
-    pass
+    # Проверить, есть ли категория с таким наименованием
+    if category_repository.is_category_exists(category.name):
+
+        # Если категория уже есть, выбрасываем исключение
+        raise HTTPException(
+            status_code=409, detail=f"Category '{category.name}' already exists."
+        )
+
+    # Иначае создать категорию
+    new_category = category_repository.create_category(category)
+
+    # Вернуть новую категорию
+    return new_category
+
+
+def get_category_by_id(id: int):
+    # Проверить, отсутствует ли категория с таким идентификатором
+    if not category_repository.is_category_exists(id):
+
+        # Если категории нет, выбрасываем исключение
+        raise HTTPException(
+            status_code=404, detail=f"Category with given id'{id}' does not exist."
+        )
+
+    # Иначе вернуть категорию
+    return category_repository.get_category_by_id(id)
+
+
+def get_all_categories():
+    # Вернуть все категории
+    return category_repository.get_all_categories()
+
+
+def update_category(category: GetCategory):
+    # Проверить, есть ли категория с таким названием
+    if category_repository.is_category_exists(category.name):
+
+        # Если категория уже есть, выбрасываем исключение
+        raise HTTPException(
+            status_code=409,
+            detail=f"Category with given name '{category.name}' already exists.",
+        )
+
+    # Иначае обновить категорию
+    updated = category_repository.update_category(category)
+    # Вернуть измененную категорию
+    return updated
+
+
+def delete_category(id: int):
+    # Получить категорию по id
+    category = get_category_by_id(id)
+
+    # Проверить, есть ли в магазине продукты данной категории
+    if product_repository.is_product_exists_by_category(category):
+
+        # Если такие продукты есть, выбрасываем исключение
+        raise HTTPException(
+            status_code=409,
+            detail=f"There are products with given category f{category.name} in store",
+        )
+
+    # Иначе удалить категорию
+    category_repository.delete_category(id)
+    return {"message": f"Category with given id '{id}' successfully deleted"}
