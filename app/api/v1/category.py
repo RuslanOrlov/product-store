b1538f9b840e68d61ecca07b27d3e344646f2321
Ruslan Orlov <org1978org@gmail.com>
2026-09-10 23:03:14 +0500

    modified: app/api/v1/category.py

--- app/api/v1/category.py
+++ app/api/v1/category.py
@@ -10,28 +10,30 @@ router = APIRouter()
 
 @router.post("/create")
 def create_category(
-    category: CreateCategoryRequest, db: Session = Depends(get_db)
+    category: CreateCategoryRequest,
+    db: Session = Depends(get_db),  # noqa: B008
 ) -> GetCategory:
     return category_service.create_category(db, category)
 
 
 @router.get("/{id}")
-def get_category_by_id(id: int, db: Session = Depends(get_db)) -> GetCategory:
+def get_category_by_id(id: int, db: Session = Depends(get_db)) -> GetCategory:  # noqa: B008
     return category_service.get_category_by_id(db, id)
 
 
 @router.get("/all")
-def get_all_categories(db: Session = Depends(get_db)) -> list[GetCategory]:
+def get_all_categories(db: Session = Depends(get_db)) -> list[GetCategory]:  # noqa: B008
     return category_service.get_all_categories(db)
 
 
 @router.put("/update")
 def update_category(
-    category: GetCategory, db: Session = Depends(get_db)
+    category: GetCategory,
+    db: Session = Depends(get_db),  # noqa: B008
 ) -> GetCategory:
     return category_service.update_category(db, category)
 
 
 @router.get("/{id}/delete")
-def delete_category(id: int, db: Session = Depends(get_db)) -> dict[str, str]:
+def delete_category(id: int, db: Session = Depends(get_db)) -> dict[str, str]:  # noqa: B008
     return category_service.delete_category(db, id)
